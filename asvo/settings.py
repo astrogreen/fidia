3123f897048547e7e476812f4c3c9e74632fe54b
Lloyd Harischandra <lharischandra@aaomc56lhar.aao.gov.au>
2015-09-07 12:24:02 +1000

    ASVO-237 #time 30m #comment Changed database configurations to connect to a postgressql db

--- asvo/settings.py
+++ asvo/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'asvo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'asvo',
+        'USER': 'asvo',
+        'PASSWORD': 'a1s9v8o4!P',
+        'HOST': 'localhost',
     }
 }
 
