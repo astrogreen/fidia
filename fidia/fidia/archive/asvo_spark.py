b51fad78b082f2de303239e9ed24cd3dbfb40203
Andy Green <andrew.green@aao.gov.au>
2015-09-23 15:57:40 +1000

    FIDIA Now can create a sample from a Spark query, and return tabular data for that sample.
    Learned A LOT! Hashed out a fair bit of how FIDIA will work as well. Much of the code here is very half baked, but it achieves the goal.
    
    This is the demonstrator code:
    
    ```python
    from fidia import Sample, AstronomicalObject
    from fidia.archive.asvo_spark import AsvoSparkArchive
    
    query = u"SELECT cataid, z, metal FROM stellarmasses WHERE z < 0.05 AND z > 0.0499"
    
    mysample = AsvoSparkArchive().new_sample_from_query(query)
    
    mysample.tabular_data() # Returns a Pandas dataframe with the tabular data.
    
    mysample.tabular_data().to_csv()
    mysample.tabular_data().to_html()
    
    ```
    
    Things that work/have been achieved:
    - The tabular data is stored with each archive object
    - Sample knows how to combine tabular data from all attached archive
    
    Things that don't work/todo:
    - AstroObject will have to be taught how to access data by passing requests to the corresponding archive
    - Currently, sample doesn't correctly know how to create AstroObjects (which will only be created on demand)
    - The property is completely unused. So the data is very "dumb"

---
+++ fidia/fidia/archive/asvo_spark.py
@@ -0,0 +1,83 @@
+"""
+A FIDIA archive plugin to support the initial efforts of the AAT ASVO Node.
+
+
+
+"""
+
+__author__ = "Andy Green"
+
+
+# External package imports
+import pandas as pd
+
+# Custom Packages
+from asvo_spark import hive_context
+
+# Relative imports in this package
+from .base_archive import BaseArchive
+from ..sample import Sample
+
+class AsvoSparkArchive(BaseArchive):
+
+    def __init__(self):
+        super(AsvoSparkArchive, self).__init__()
+
+        # Store connection to spark.
+        self._hive_context = hive_context
+
+        self.contents = dict()
+
+
+
+    def writeable(self):
+        return False
+
+    @property
+    def default_object(self):
+        return None
+
+    @property
+    def name(self):
+        return 'ASVO'
+    
+
+    def add_object(self, value):
+    	pass
+
+    def new_sample_from_query(self, query_string):
+        """Create a new FIDIA sample containing the query results.
+
+        @TODO For now, this assumes that the first column contains the
+        @identifier.
+
+        """
+
+        # Get Query Results from Spark (as a SchemaRDD object):
+        results_rdd = hive_context.sql(query_string)
+
+        # Convert to a Pandas DataFrame
+        results_pddf = results_rdd.toPandas()
+
+        # Set the key to be the first column
+        results_pddf.set_index(results_pddf.columns[0], inplace=True)
+
+        # Create an empty sample, and populate it via it's private interface
+        # (no suitable public interface at this time.)
+        new_sample = Sample()
+        id_cross_match = pd.DataFrame(pd.Series(results_pddf.index, 
+                    name='ASVO',
+                    index=results_pddf.index))
+        print(id_cross_match)
+        new_sample.extend(id_cross_match)
+
+        self.tabular_data = results_pddf
+
+        new_sample._archives = {self}
+
+        # Finally, we mark this new sample as immutable.
+        new_sample.mutable = False
+
+        return new_sample
+
+
--- fidia/fidia/astro_object.py
+++ fidia/fidia/astro_object.py
@@ -1,6 +1,10 @@
-class AstronomicalObject(object):
 
-    def __init__(self, identifier=None, ra=None, dec=None):
+import collections
+
+
+class AstronomicalObject(collections.MutableMapping):
+
+    def __init__(self, sample, identifier=None, ra=None, dec=None):
         if identifier is None:
             if ra is None or dec is None:
                 raise Exception("Either 'identifier' or 'ra' and 'dec' must be defined.")
@@ -8,9 +12,17 @@ class AstronomicalObject(object):
         else:
             self._identifier = identifier
 
+        # Reference to the sample containing this object.
+        self.sample = sample
+
+        # Dictionary of IDs for this object in the various archives attached
+        # to the sample. @TODO: Initialised to None: must be populated
+        # seperately.
+        self._archive_id = {archive: None for archive in sample.archives}
+
         self._ra = ra
         self._dec = dec
-
+        super(AstronomicalObject, self).__init__()
 
     @property
     def identifier(self):
@@ -24,5 +36,36 @@ class AstronomicalObject(object):
     def dec(self):
         return self._dec
     
-    
-     
+
+    # ____________________________________________________________________
+    # Functions to create dictionary like behaviour
+    #
+    #     These are required as part of the collections.MutableMapping class.
+
+    def __getitem__(self, key):
+        """Function called on dict type read access"""
+
+        if isinstance(key, list):
+            # We have been asked for more than one property, will return tabular data?
+            # @TODO!
+            raise Exception("List indexing behaviour not implemented.")
+        else:
+            raise Exception("Key indexing behaviour not implemented.")
+            # # Asked for a single property
+            # archive = self.sample.get_archive_for_property(key)
+            # archive.data.loc[self._archive_id[archive]][key]
+
+    def __setitem__(self, key, value):
+        pass
+
+    def __delitem__(self, key):
+        pass
+
+    def keys(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __iter__(self):
+        pass
