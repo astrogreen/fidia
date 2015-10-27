5b0746bf5ff30cc518f688e3865e202abc903d56
Liz <liz.mannering@aao.gov.au>
2015-10-27 12:38:52 +1100

    formatted results data and added plugin to add functionality to the table. fixed several layout bugs. beginning work on rendering the bound form after submit to show user the query submitted

--- static/js/main.js
+++ static/js/main.js
@@ -218,11 +218,23 @@ $( document ).ready(function() {                                    //FORM INITI
     //JQUERY VALIDATION
 //TODO test plugin - cost??!
 
+
 //DOCS
 $('body').scrollspy({
     target: '.docs-sidebar',
     offset: 40
 });
+
+//DATA TABLES
+
+if ($('#returnTableResults > table > thead > tr:nth-child(2) > th:nth-child(1)').length){
+//get text and remove dummy thead tr - problem with data structure in panda
+    $('table > thead > tr:nth-child(1) > th:nth-child(1)').append($('table > thead > tr:nth-child(2) > th:nth-child(1)').html());
+    $('#returnTableResults > table > thead > tr:nth-child(2)').remove();
+    $('#returnTableResults > table').css( 'border', '1px solid #ddd' );
+    $('#returnTableResults > table').DataTable();
+};
+
 
 
 });
