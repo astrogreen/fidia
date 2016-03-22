7b12da493b15a4708547347485c8163fcd0215bf
Andy Green <andrew.green@aao.gov.au>
2016-03-22 13:25:59 +1100

    Working version of JSON GAMA table descriptions, but slow.
    This is a first go at ASVO-417, but querying via Hive seems to be very slow. On my machine, this takes many 10s of seconds. Not sure why it is so slow. Manually running some of the queries against pyspark shows that they run faster there, but the overhead still makes them slow. Could this just be that Hive is using MapReduce, and Spark is smarter?
    
    Best plan is probably to just precreate the file and then serve it statically. So just such a file has been added to the rest_api app.
    
    ASVO-417 #time 2h 30m

---
+++ python_packages/asvo_database_backend_helpers.py
@@ -0,0 +1,105 @@
+import json
+from collections import OrderedDict
+from impala.dbapi import connect
+
+from cached_property import cached_property
+
+def singleton(cls):
+    instance = cls()
+    instance.__call__ = lambda: instance
+    return instance
+
+@singleton
+class GAMADatabase:
+
+    def __init__(self):
+        self.conn = None
+        self.cursor = None
+        self.table_description_cache = dict()
+
+    def open_connection(self):
+        self.conn = connect(host='asvotest1.aao.gov.au', port=10000, auth_mechanism='PLAIN')
+        self.cursor = self.conn.cursor()
+
+    @cached_property
+    def tables(self):
+        if self.conn is None:
+            self.open_connection()
+
+        self.cursor.execute("SELECT name FROM tables WHERE tableid IS NOT NULL")
+        tables = [row[0] for row in self.cursor]
+        return tables
+
+    def describe_gama_table(self, table_name):
+
+        if table_name in self.table_description_cache:
+            return self.table_description_cache[table_name]
+
+        if self.conn is None:
+            self.open_connection()
+        if table_name not in self.tables:
+            raise ValueError("Table '%s' not in GAMA database" % table_name)
+
+        # Collect the meta data
+
+        #self.cursor.execute("DESCRIBE dmus")
+        #columns = [row[0] for row in self.cursor]
+        columns = OrderedDict({"name": "tables.name",
+                               "catalogId": "tables.tableid",
+                               "version": "tables.version",
+                               "DMU": "dmus.name",
+                               "description": "dmus.shortdescription",
+                               "contact": "tables.contact"
+                               })
+
+        self.cursor.execute("SELECT {columns} FROM tables JOIN dmus ON (tables.dmuid = dmus.dmuid ) WHERE tables.name = '{table_name}'".format(
+            columns=", ".join(columns.values()),
+            table_name=table_name))
+
+        data = self.cursor.fetchall()
+        if len(data) != 1:
+            print("Data not the right length:")
+            for i, d in enumerate(data):
+                print("    " + str(i) + ": " + str(d))
+            assert len(data) != 1, "Multiple or no GAMA tables found for table name '%s'" % table_name
+
+        gama_table = dict()
+        for column, value in zip(columns, data[0]):
+            gama_table[column] = value
+
+
+
+        # Collect information on the columns
+
+        columns = ("columnid", "tableid", "name", "units", "ucd", "description")
+
+        self.cursor.execute("SELECT {columns} FROM columns WHERE tableid = '{tableid}'".format(
+            columns=", ".join(columns),
+            tableid=gama_table['catalogId']))
+        column_info = list()
+        for cid, row in enumerate(self.cursor):
+            column_dict = {col: value for col, value in zip(columns, row)}
+            column_info.append(column_dict)
+
+        gama_table['columns'] = column_info
+
+        self.table_description_cache[table_name] = gama_table
+
+        return gama_table
+
+def get_gama_database_as_json():
+
+    schema = []
+    for table in GAMADatabase.tables:
+        schema.append(GAMADatabase.describe_gama_table(table))
+
+    return json.dumps(schema)
+
+if __name__ == '__main__':
+    print("\n\nTables found are:")
+    for t in GAMADatabase.tables:
+        print("   " + t)
+    desc = GAMADatabase.describe_gama_table("StellarMasses")
+    print("\n\nDescription of StellarMasses:")
+    for r in desc:
+        print("    " + r + ": " + str(desc[r]))
