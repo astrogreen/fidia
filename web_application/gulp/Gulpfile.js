020fc1b487ed74376671200b5c4a8007b2f7c143
Liz <liz.mannering@aao.gov.au>
2016-06-07 13:00:22 +0800

    Final cleanup after refactor into apps, gulp file changed to look at new directory structure. Gulp: watches files in app/static/app/less/component/ and app/static/app/less/ minifies to app/static/app/css/component and app/static/app/css/
    #ASVO-543 #time 1h #in-progress

--- web_application/gulp/Gulpfile.js
+++ web_application/gulp/Gulpfile.js
@@ -39,27 +39,43 @@ var gulp = require('gulp'),
 
 
 // Default task
-gulp.task('default', function(){
-    gulp.start('styles'); 
-});
+// gulp.task('default', function(){
+//     gulp.start('styles');
+// });
 
 // Watch
 gulp.task('watch', function(){
     // watch the css files in the less directory (transpiler will update if less is altered)
     // minify and move to css/folder/
-    gulp.watch(['restapi_app/static/restapi_app/less/*.css', 'restapi_app/static/restapi_app/less/**/*.css'], function(file){
+    gulp.watch(['../**/static/**/less/*.css', '../**/static/**/less/**/*.css'], function(file){
     })
         .on("change", function(file){
-            return gulp.src(file.path)
+            console.log(file);
+
+            var full_path= file.path;
+            var static_path = full_path.split("less/")[0];
+            var css_path = static_path+'css/';
+            var less_path = static_path+'less/';
+            var file_name = full_path.split("/").pop();
+            var component_name = file_name.split(".css")[0];
+            var new_dest = css_path+component_name+'/';
+
+            var paths = {
+                full_path: full_path,
+                static_path: static_path,
+                less_path: less_path,
+                css_path: css_path,
+                file_name: file_name,
+                component_name: component_name,
+                new_dest: new_dest
+            };
+            console.log(paths);
+            console.log('----');
+            return gulp.src(paths.full_path)
                 .pipe(autoprefixer('last 2 version'))
-                .pipe(rename(function(file){
-                    // join the base name (e.g., main) to the file path, effectively forcing gulp to create the folder if it doesn't
-                    // exist already
-                    file.dirname = path.join(file.dirname, file.basename)
-                }))
                 .pipe(rename({suffix: '.min'}))
                 .pipe(cssnano())
-                .pipe(gulp.dest('restapi_app/static/restapi_app/css/'))
-                .pipe(notify({ message: 'Styles task complete' }));
+                .pipe(gulp.dest(paths.new_dest))
+                .pipe(notify({message: 'Gulp task complete: '+paths.new_dest}));
         })
 });
