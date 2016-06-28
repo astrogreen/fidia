59a97ecd0a23015830b2fccca3b8663fe223efcb
Andy Green <andrew.green@aao.gov.au>
2016-06-28 13:22:21 +1000

    Another attempt to make the descriptive data work the way I want.
    In this attempt, I thought I could take advantage of the way python descriptors were implemented to keep the descriptor object, but still set to it at the class level. However `__set__` doesn't provide the functionality I thought it did. This code is basically incomplete (and the corresponding test is failing.)
    
    So, I'm going to go a different route for the moment: simply make the descriptors attributes with no smarts (see next commit.)
    
    ASVO-571 #time 45m

--- python_packages/fidia/descriptions.py
+++ python_packages/fidia/descriptions.py
@@ -1,27 +1,33 @@
+from . import slogging
+log = slogging.getLogger(__name__)
+log.enable_console_logging()
+log.setLevel(slogging.DEBUG)
 
 
 class DescriptionsDescriptor:
-    def __init__(self, string=None):
-        self.contents = string
 
-    def __get__(self, instance, owner):
-        if instance is None:
-            return self
-        else:
-            if self.contents is not None:
-                return self.contents
-            else:
-                return self.get_contents(instance, owner)
+    def __get__(self, instance, klass):
+        try:
+            if instance is None:
+                return klass._descriptive_data[self.attribute_name]
+            elif instance is not None:
+                return instance._descriptive_data[self.attribute_name]
+        except (KeyError, AttributeError):
+            return self.get_contents(instance, klass)
 
-    def get_contents(self, instance, owner):
+    def __set__(self, instance, value):
+        raise Exception("Descriptive information cannot be set in this way. Programming Error.")
+
+    def get_contents(self, instance, klass):
         return None
 
 class PrettyName(DescriptionsDescriptor):
-    pass
+    attribute_name = "pretty_name"
 
 
 class Description(DescriptionsDescriptor):
-    def get_contents(self, instance, owner):
+    attribute_name = "description"
+    def get_contents(self, instance, klass):
         try:
             if hasattr(instance, 'doc') and instance.doc is not None:
                 if "\n" in instance.doc:
@@ -38,11 +44,28 @@ class Description(DescriptionsDescriptor):
 
 
 class Documentation(DescriptionsDescriptor):
-    def get_contents(self, instance, owner):
+    attribute_name = "documentation"
+    def get_contents(self, instance, klass):
+        if instance is None:
+            object = klass
+        else:
+            object = instance
+
         try:
-            if hasattr(instance, 'doc') and instance.doc is not None:
-                return instance.doc
-            if hasattr(instance, '__doc__') and instance.__doc__ is not None:
-                return instance.__doc__
+            if hasattr(object, 'doc') and object.doc is not None:
+                return object.doc
+            if hasattr(object, '__doc__') and object.__doc__ is not None:
+                return object.__doc__
         except:
             return None
+
+class ShortName(DescriptionsDescriptor):
+    attribute_name = "short_name"
+
+    def get_contents(self, instance, klass):
+        if instance is None:
+            object = klass
+        else:
+            object = instance
+
+        return object.__name__
