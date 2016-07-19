2b87b79b57b8f6a444f171751974cac8c4fe1d17
Andy Green <andrew.green@aao.gov.au>
2016-07-19 16:47:11 +1000

    Reinserted accidentally deleted function `trait_property_from_fits_header` from previous merge.
    There are no tests for this function. This is used by sami archive: this should be fixed up!

--- python_packages/fidia/traits/utilities.py
+++ python_packages/fidia/traits/utilities.py
@@ -353,3 +353,17 @@ class BoundTraitProperty:
 # `isinstance(value, TraitProperty)` work as expected for `TraitProperty`s that have
 # been bound to their `Trait`.
 TraitProperty.register(BoundTraitProperty)
+
+def trait_property_from_fits_header(header_card_name, type, name):
+
+    tp = TraitProperty(type=type, name=name)
+
+    tp.fload = lambda self: self._header[header_card_name]
+    tp.short_name = header_card_name
+
+    # # @TODO: Providence information can't get filename currently...
+    # tp.providence = "!: FITS-Header {{file: '{filename}' extension: '{extension}' header: '{card_name}'}}".format(
+    #     card_name=header_card_name, extension=extension, filename='UNDEFINED'
+    # )
+
+    return tp
