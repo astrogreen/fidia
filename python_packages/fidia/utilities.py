99f5d9483deb70ff9e9b3e69c93fee2d563440aa
Andy Green <andrew.green@aao.gov.au>
2016-07-21 14:39:46 +1000

    Added a file based locking context manager to support multi-thread/process safe operations.
    Note the original implementation of this used the "contextmanager" decorator from the contextlib package, but it didn't seem to correctly run the __exit__ method, so this is implemented in the traditional way.

--- python_packages/fidia/utilities.py
+++ python_packages/fidia/utilities.py
@@ -1,5 +1,14 @@
 from copy import deepcopy
 from collections import Iterable, Sized
+from contextlib import contextmanager
+import os
+import errno
+from time import sleep
+
+from . import slogging
+log = slogging.getLogger(__name__)
+log.setLevel(slogging.DEBUG)
+log.enable_console_logging()
 
 def none_at_indices(tup, indices):
     result = tuple()
@@ -142,3 +151,56 @@ class Default: pass
 def is_list_or_set(obj):
     """Return true if the object is a list, set, or other sized iterable (but not a string!)"""
     return isinstance(obj, Iterable) and isinstance(obj, Sized)
+
+# @contextmanager
+def exclusive_file_lock(filename):
+    """A context manager which will block while another process holds a lock on the named file.
+
+    While another process is executing this context, this process will block,
+    and only start executing the context once the lock has been released.
+
+    Adapted from http://stackoverflow.com/questions/30407352/how-to-prevent-a-race-condition-when-multiple-processes-attempt-to-write-to-and
+
+    """
+
+    class LockFileContextManager:
+
+        lockfilename = filename + '.LOCK'
+
+        def __enter__(self):
+
+            # Loop until a lock file can be created:
+            lock_aquired = False
+            n_waits = 0
+            while not lock_aquired:
+                try:
+                    fd = os.open(self.lockfilename, os.O_CREAT | os.O_EXCL | os.O_EXLOCK | os.O_WRONLY)
+                except OSError as e:
+                    if e.errno != errno.EEXIST:
+                        raise
+                    else:
+                        # Wait some time for the lock to be cleared before trying again.
+                        sleep(0.5)
+                        n_waits += 1
+                        if n_waits == 10:
+                            log.warning("Waiting for exclusive lock on file '%s'", self.lockfilename)
+                        if n_waits == 10:
+                            log.warning("Still waiting for exclusive lock on file '%s': stale lockfile?",
+                                        self.lockfilename)
+
+                except:
+                    raise
+                else:
+                    lock_aquired = True
+
+            # Lock has been aquired. Open the file
+            self.f = os.fdopen(fd)
+
+            return
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            # Context complete. Release the lock.
+            os.remove(self.lockfilename)
+            self.f.close()
+
+    return LockFileContextManager()
