628e44bd2c5ea637081d3c32282020a0d0be9de0
Liz <liz.mannering@aao.gov.au>
2016-07-28 13:06:54 +0800

    JSON validation on form field - added validation message catching (response.data.non_form_errors) to api.html. The process of catching all status codes and error messages is handled in the template - probably needs to be abstracted
    #ASVO-562 #time 30m #in-progress

--- web_application/download/models.py
+++ web_application/download/models.py
@@ -7,9 +7,10 @@ class Download(models.Model):
     created = models.DateTimeField(auto_now_add=True, editable=False)
     updated = models.DateTimeField(auto_now=True, editable=False)
     title = models.CharField(max_length=100, blank=True, default="My Download")
-    # items = models.CharField(max_length=100, blank=True, default="My Items")
-    items = JSONField(default="")
+    # items = models.CharField(max_length=2000, blank=False, default="")
+    items = JSONField(default="", blank=False)
     owner = models.ForeignKey('auth.User', related_name='download')
+    downloadlink = models.CharField(max_length=150, blank=True, default="In Progress")
 
     class Meta:
         ordering = ('created',)
--- web_application/download/serializers.py
+++ web_application/download/serializers.py
@@ -1,3 +1,4 @@
+import json
 from rest_framework import serializers
 import download.models
 
@@ -6,12 +7,19 @@ class DownloadSerializer(serializers.HyperlinkedModelSerializer):
     """
     Download Viewset serializer
     """
+    def validate(self, data):
+        try:
+            json_object = json.loads(data['items'])
+        except ValueError:
+            raise serializers.ValidationError('Submitted data is not valid json')
+
     owner = serializers.ReadOnlyField(source='owner.username')
     updated = serializers.DateTimeField(format="%Y-%m-%d, %H:%M:%S", read_only=True)
     title = serializers.CharField(default='My Download', max_length=100)
     created = serializers.DateTimeField(format="%Y-%m-%d, %H:%M:%S", read_only=True)
-    # items = serializers.JSONField(required=True, allow_null=False)
+    items = serializers.JSONField(required=True, allow_null=False)
+    downloadlink = serializers.URLField(required=False, max_length=150)
 
     class Meta:
         model = download.models.Download
-        fields = ('title', 'items', 'owner', 'url', 'created', 'updated')
+        fields = ('title', 'items', 'owner', 'url', 'created', 'updated', 'downloadlink')
