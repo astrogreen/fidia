b5c2095eac75c75b9a6a280571ee68aa3686c46b
Liz <liz.mannering@aao.gov.au>
2016-08-10 13:13:27 +0800

    Remove console.log()
     #ASVO-602 #time 1h #ready-for-testing

--- web_application/restapi_app/static/restapi_app/js/panel-toggle-caret/panel-toggle-caret.js
+++ web_application/restapi_app/static/restapi_app/js/panel-toggle-caret/panel-toggle-caret.js
@@ -2,10 +2,8 @@ $(".panel-group .panel-heading a")
         .each(function () {
             if ($(this).children('i').hasClass('fa-caret-down')) {
                 var pointer = $(this).attr('href').toString();
-                console.log(pointer);
                 // If open, switch the caret to upwards-facing
                 if ($(pointer).hasClass('in')) {
-                    console.log('EXPANDED');
                     $(this).children('i.fa-caret-down').removeClass('fa-caret-down').addClass('fa-caret-up');
                 }
             }
