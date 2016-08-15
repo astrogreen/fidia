31adb68c18a4f085dd12b56e6c7804a1ac7aef49
Liz <liz.mannering@aao.gov.au>
2016-08-15 21:36:53 +0800

    Remove console.log statement
     #ASVO-593 #time 1m

--- web_application/download/static/download/js/directives.js
+++ web_application/download/static/download/js/directives.js
@@ -97,11 +97,13 @@ console.log('directives.js');
                             var item ={};
                             var temp = attr.url;
                             // ?format=type
+
                             item['id']=temp+'?format='+scope.formats[f];
-                            console.log(attr)
-                            item['options'] = attr.options;
-                            item['prettyname'] = attr.prettyname;
                             item['format'] = scope.formats[f];
+
+                            if (attr.options){item['options'] = attr.options;};
+                            if (attr.prettyname){item['prettyname'] = attr.prettyname;};
+
                             // Shape of objects is as: scope.items ["json"] = item == {id: "http://127.0.0.1:8000/asvo/data-browser/sami/9352/spectral_map-red/?format=json", options: "", format: "json"}
                             scope.items[scope.formats[f]] = item;
                         }
