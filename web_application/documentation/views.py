ee13b0a26b22092ff0aa7a3d1b638ec3bf40d6d0
Liz <liz.mannering@aao.gov.au>
2016-09-21 14:13:43 +0800

    TinyMCE no longer stripping <img> tags. Sidebar for documentation routes.
     This allows us to use images (from the static folder) in the
     documentation.
    
     Documentation moved over to new sidebar format. Article serializer/model
     returns a list of article slugs (filtered by topic-type) to article
     resource page for sidebar menu.
    
     Permissions altered such that only _is_staff can edit docs (obj+group
     permissions not used here now, due to the fact that we'll likely
     move over to a full CMS).
    
    #ASVO-674 #time 20m #done #ASVO-675 #time 3h #done

--- web_application/documentation/views.py
+++ web_application/documentation/views.py
@@ -41,7 +41,13 @@ class ArticleViewset(viewsets.ModelViewSet):
     serializer_class = documentation.serializers.ArticleSerializer
     queryset = documentation.models.Article.objects.all()
     lookup_field = 'slug'
-    permission_classes = [restapi_app.permissions.IsSurveyTeamOrAdminElseReadOnly]
+    # permission_classes = [restapi_app.permissions.IsSurveyTeamOrAdminElseReadOnly]
+    permission_classes = [restapi_app.permissions.IsAdminOrReadOnly]
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return documentation.serializers.ListArticleSerializer
+        return documentation.serializers.ArticleSerializer
 
     class ArticleRenderer(restapi_app.renderers.ExtendBrowsableAPIRenderer):
         template = 'documentation/article.html'
--- web_application/restapi_app/permissions.py
+++ web_application/restapi_app/permissions.py
@@ -39,6 +39,7 @@ class IsNotAuthenticated(permissions.BasePermission):
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
+
         return (
             request.method in permissions.SAFE_METHODS or
             request.user and
@@ -49,7 +50,15 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 class IsSurveyTeamOrAdminElseReadOnly(permissions.DjangoObjectPermissions):
     """ Admin accesses all actions, if user group matches object 'edit_group' field,
         then grant temporary access to the update action on this object. """
+
+    def has_permission(self, request, view):
+
+        return (request.method in permissions.SAFE_METHODS or
+                request.user and request.user.is_staff
+                )
+
     def has_object_permission(self, request, view, obj):
+        print('IsSurveyTeamOrAdminElseReadOnly: OBJECT')
         _is_survey_team = False
 
         for g in request.user.groups.all():
@@ -57,7 +66,11 @@ class IsSurveyTeamOrAdminElseReadOnly(permissions.DjangoObjectPermissions):
                 if hasattr(obj.edit_group, 'name'):
                     if obj.edit_group.name == g.name:
                         _is_survey_team = True
-
+        print(
+            request.method in permissions.SAFE_METHODS or
+            request.user and request.user.is_staff or
+            request.user and _is_survey_team and request.method == 'PUT'
+        )
         return (
             request.method in permissions.SAFE_METHODS or
             request.user and request.user.is_staff or
