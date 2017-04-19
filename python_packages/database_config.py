711c411dfeca8dcc7f6f26c7e271594482c7409a
Liz <liz.mannering@aao.gov.au>
2017-04-19 12:06:00 +0800

    Re-tracking database_config (custom_database_config will locally host  settings) #ASVO-955 #time 1m #in-progress

--- python_packages/database_config.py
+++ python_packages/database_config.py
@@ -1,14 +1,17 @@
-local_default = 'sqlite'  # values: sqlite, postgres
+local_default = 'postgres'  # values: sqlite, postgres
 local_postgres = {'host': 'localhost',
-                  'user': 'myuser',
-                  'passwd': 'my secret password',
-                  'db': 'mydb'}
+                  'user': 'lmannering',
+                  'passwd': '987dfkjshdf!p',
+                  'db': 'aaodc'}
 
 local_sqlite = {'file': 'db file path'}
 
-postgres_svr = {'host': 'server name',
-                'user': 'myuser',
-                'passwd': 'my secret password',
-                'db': 'mydb'}
+postgres_svr = {'host': 'asvotest1.aao.gov.au',
+                'user': 'asvo',
+                'passwd': 'a1s9v8o4!P',
+                'db': 'aaodc'}
 
-
+try:
+    from custom_database_config import *
+except ImportError:
+    pass
