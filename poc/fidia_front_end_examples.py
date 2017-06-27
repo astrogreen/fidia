3b8841475be9e0a1777c9ec2c6dca14861e1a7a1
Andy Green <andrew.green@aao.gov.au>
2017-06-27 17:01:20 +1000

    Added a detailed object view, which perhaps should eventually be populated from an actual fidia.AstroObject.
    branch: sov-fidia_v0.4

--- poc/fidia_front_end_examples.py
+++ poc/fidia_front_end_examples.py
@@ -13,6 +13,25 @@ def get_all_known_object_names_in_any_archive():
         all_objects.extend(archive.contents)
     return all_objects
 
+def get_detailed_object_information_for_all_objects_in_any_archive():
+    """Get all known objects with extra information for each.
+
+    Though we'll have to do benchmarking and some other work, but this is
+    probably 10x-100x slower than the previous function.
+
+    """
+
+    all_objects = []
+    for archive in fidia.known_archives.all:
+        for object_id in archive.contents:
+            # @TODO: Replace with an actual astro_object from the archive? Must this be serialized?
+            all_objects.append({
+                "object_id": object_id,
+                "adcid_id": "adcid_id",
+                "archive": archive.archive_id
+            })
+    return all_objects
+
 def get_dictionary_of_known_archives():
     """Get a dictionary containing information about all known archives."""
 
@@ -53,6 +72,9 @@ if __name__ == '__main__':
 
     print_call(get_all_known_object_names_in_any_archive)
 
+    print_call(get_detailed_object_information_for_all_objects_in_any_archive)
+
     print_call(get_dictionary_of_known_archives)
 
     print_call(get_top_level_traits_for_archive, "ExampleArchive")
+
